b7548ce463cea15ecb807fdb097ec93520bf7858
fuzzygwalchmei <marc.falzon@gmail.com>
2017-09-06 23:42:22 -0700

    Roguelike added

--- OpeningExcel.py
+++ OpeningExcel.py
@@ -1,10 +1,35 @@
-import openpyxl
+import mysql
+import csv
 
-wb = openpyxl.load_workbook('Billable_Suppliers.xlsx')
-wb.get_sheet_names()
-print(wb)
-ActSheet = wb.active
-for rowOfCellObjects in ActSheet['A2':'C55']:
-    for cellObj in rowOfCellObjects:
-        print(cellObj.coordinate, cellObj.value),
-    print('--- END OF ROW ---')
+csv_data = csv.reader('/Fuzzle/Documents/all_suppliers.csv')
+
+database = mysql.connector.connect (database = "demodb", user="root@localhost", password="MySQLPassword", host="localhost", port="3306")
+
+cursor = database.cursor()
+delete = """Drop table if exists Real.SampleDataTwo"""
+print (delete)
+
+mydata = cursor.execute(delete)
+
+cursor.execute("""Create Table Real.SampleDataTwo
+                (User varchar(55),
+                LastUpdate timestamp,
+                Week date,
+                Builder varchar(55),
+                Traffic integer
+                );""")
+
+print("Table created successfully")
+
+for row in csv_data:
+
+    cursor.execute("INSERT INTO Real.SampleDataTwo (User, LastUpdate, Week, Builder, Traffic)"\
+                "VALUES (%s,%s,%s,%s,%s)",
+               row)
+
+
+cursor.close()
+database.commit()
+database.close()
+
+print ("CSV data imported")
--- TkinterTest.py
+++ TkinterTest.py
@@ -2,24 +2,25 @@ from tkinter import *
 
 root = Tk()
 
-def clickButton(button,row,col):
+def clickButton(button):
     #do something
     if button['text'] == "#":
-        myInfo = button.grid_info(); #myRow = ; myCol = myInfo['column']
+        myInfo = button.grid_info();
         button.config(text = myArray[myInfo['row']][myInfo['column']])
     else:
         button.config(text="#")
+    myLabel.config(text=(myInfo['row'],myInfo['column'],myArray[myInfo['row']][myInfo['column']]))
 
-myArray = [(1,2,3),(4,5,6),(7,8,9)]
+myArray = [(1,2,3),(4,5,6),(7,8,9),(1,2,3),(4,5,6),(7,8,9)]
 
 for each in myArray:
     for item in each:
         myRow = myArray.index(each); myCol = each.index(item)
         print(myRow,":",myCol,":",item)
         b=Button(root,text=item)
-        b.config(command=lambda b=b: clickButton(b,myRow,myCol))
+        b.config(command=lambda b=b: clickButton(b))
         b.grid(row=myRow, column=myCol)
 
-
+myLabel = Label(root).grid(row=3,columnspan=3)
 
 mainloop()
