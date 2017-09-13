362314d689c5830da9854783408c1236ef04fb8a
fuzzygwalchmei <marc.falzon@gmail.com>
2017-09-12 23:55:05 -0700

    Commiting

--- ConsoleTest.py
+++ ConsoleTest.py
@@ -1,38 +1,9 @@
 import tdl
 import textwrap
-
-#Screen Dimensions
-SCREEN_WIDTH = 150;SCREEN_HEIGHT=100
-
-#Dimensions for each Frame
-MAP_WIDTH=100;MAP_HEIGHT=100
-STAT_WIDTH=50;STAT_HEIGHT=50
-ITEM_WIDTH=50;ITEM_HEIGHT=50
-
-STAT_DATA={"Class":"Hero","Lvl":1,"HP":15}
-
-
-
-root = tdl.init(SCREEN_WIDTH,SCREEN_HEIGHT,"Main Window",fullscreen=False)
-
-ConMain = tdl.Console(MAP_WIDTH,MAP_HEIGHT)
-ConStat = tdl.Console(STAT_WIDTH,STAT_HEIGHT)
-ConItem = tdl.Console(ITEM_WIDTH,ITEM_HEIGHT)
-
-msg_counter = 2
-for each in STAT_DATA:
-    text_line = each + ": " + str(STAT_DATA[each])
-    ConStat.draw_str(5 ,msg_counter,text_line,bg=None,fg=(255,255,255))
-    msg_counter +=1
-
-
-ConMain.draw_frame(0 ,0 ,MAP_WIDTH,MAP_HEIGHT,None,bg=(255,0,0))
-ConStat.draw_frame(0 ,0 ,STAT_WIDTH,STAT_HEIGHT ,None,bg=(20,255,0))
-ConItem.draw_frame(0 ,0 ,ITEM_WIDTH ,ITEM_HEIGHT,None,bg=(0,0,255))
-
-root.blit(ConMain,0 ,0,MAP_WIDTH,MAP_HEIGHT)
-root.blit(ConStat,MAP_WIDTH ,0,STAT_WIDTH,STAT_HEIGHT)
-root.blit(ConItem,MAP_WIDTH ,STAT_WIDTH ,STAT_WIDTH,STAT_HEIGHT)
+import ConsoleConstants
+import ConsoleClasses
+import DrawScreen
 
 while not tdl.event.is_window_closed():
+    DrawScreen.Draw_Now()
     tdl.flush()
--- DrawScreen.py
+++ DrawScreen.py
@@ -1,6 +1,31 @@
 import tdl
+from ConsoleConstants import *
+import ConsoleClasses
+
+root = tdl.init(SCREEN_WIDTH,SCREEN_HEIGHT,"Main Window",fullscreen=False)
+
+ConMain = tdl.Console(MAP_WIDTH,MAP_HEIGHT)
+ConStat = tdl.Console(STAT_WIDTH,STAT_HEIGHT)
+ConItem = tdl.Console(ITEM_WIDTH,ITEM_HEIGHT)
+
+myHero = ConsoleClasses.myObjects()
 
 def Draw_Now():
     global STAT_DATA
+    msg_counter = 2
 
-    
+    myList = ("Class","level","hitPoints")
+    for each in myList:
+        valueLine = "myHero."+each
+        text_line = valueLine
+        ConStat.draw_str(5 ,msg_counter,text_line,bg=None,fg=(255,255,255))
+        msg_counter +=1
+
+
+    ConMain.draw_frame(0 ,0 ,MAP_WIDTH,MAP_HEIGHT,None,bg=(255,0,0))
+    ConStat.draw_frame(0 ,0 ,STAT_WIDTH,STAT_HEIGHT ,None,bg=(20,255,0))
+    ConItem.draw_frame(0 ,0 ,ITEM_WIDTH ,ITEM_HEIGHT,None,bg=(0,0,255))
+
+    root.blit(ConMain,0 ,0,MAP_WIDTH,MAP_HEIGHT)
+    root.blit(ConStat,MAP_WIDTH ,0,STAT_WIDTH,STAT_HEIGHT)
+    root.blit(ConItem,MAP_WIDTH ,STAT_WIDTH ,STAT_WIDTH,STAT_HEIGHT)
